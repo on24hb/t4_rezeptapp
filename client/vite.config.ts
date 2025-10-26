06d5993a8983afdec2583eb0e7b8b1c92fb143af
on24hb <han.broska.24@lehre.mosbach.dhbw.de>
2025-10-26 10:57:49 +0100

    change vite frontend to https

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'node:fs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,4 +17,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    https: {
+      key: fs.readFileSync('../server/localhost-key.pem'),
+      cert: fs.readFileSync('../server/localhost.pem'),
+    },
+    port: 5173
+  }
 })
